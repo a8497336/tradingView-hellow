888c8b2aa836aeef3045f8c7401e5d29212e2e59
a8497338 <1779840057@qq.com>
2024-12-23 22:09:51 +0800

    获取okx的获取指数K线数据

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import service from './http'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$service = service
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
